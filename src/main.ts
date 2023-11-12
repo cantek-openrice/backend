7e3ec005edf1471b68f977cd7c5c4ed63e30819f
Terry Leung <terry1651140@gmail.com>
2023-11-12 12:13:13 -0500

    setup helmet, setup api doucment

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -13,6 +15,18 @@ async function bootstrap() {
       credentials: true,
     }),
   );
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Openrice-Canada')
+    .setDescription('The Openrice-Canada API description')
+    .setVersion('1.0')
+    .addTag('openrice')
+    .build();
+
+  const doucment = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, doucment);
+
   await app.listen(8080);
 }
 bootstrap();
