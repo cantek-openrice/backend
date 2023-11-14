57cd029bc572a1dab8a11eeb9ed2258ad8de6069
ttiimmothy <dinosauli2006@gmail.com>
2023-11-13 21:36:27 -0500

    fix: delete useless codes

--- src/global/modules/knex.module.ts
+++ src/global/modules/knex.module.ts
@@ -3,7 +3,6 @@ import { Module, Global } from '@nestjs/common';
 import Knex from 'knex';
 import knexConfigs from '../../../knexfile';
 
-// const knexConfigs = require('../../../knexfile'); // must use require instead of import, from 2 years ago, now is not required
 const configMode = process.env.NODE_ENV || 'development';
 const knexConfig = knexConfigs[configMode];
 
