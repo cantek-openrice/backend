d06c0a7cc07755ecd68b6051035f00b082de3864
ttiimmothy <dinosauli2006@gmail.com>
2023-11-16 00:55:13 -0500

    fix: import path

--- src/userRelated/user/dto/create-user.dto.ts
+++ src/userRelated/user/dto/create-user.dto.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { Role } from 'src/global/utils/enums/Role';
-
-export interface CreateUserDto {
-  email: string;
-  username: string;
-  password: string;
-  role: Role;
-=======
 import { UserRole } from '../../../global/utils/enums/UserRole';
 
 export class CreateUserDto {
@@ -14,5 +5,4 @@ export class CreateUserDto {
   username: string;
   password: string;
   role?: UserRole;
->>>>>>> 1152dfa5f440c26c0775963630b280b7babcd446
 }
--- src/userRelated/user/dto/update-user.dto.ts
+++ src/userRelated/user/dto/update-user.dto.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { Role } from 'src/global/utils/enums/Role';
-
-export interface UpdateUserDto {
-  username?: string;
-  password?: string;
-  role?: Role;
-=======
 import { UserRole } from '../../../global/utils/enums/UserRole';
 
 export class UpdateUserDto {
   username?: string;
   password?: string;
   role?: UserRole;
->>>>>>> 1152dfa5f440c26c0775963630b280b7babcd446
 }
