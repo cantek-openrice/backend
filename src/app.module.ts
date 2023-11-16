725feae2a009dd64b48d0f4c38888c4ab256db1e
ttiimmothy <dinosauli2006@gmail.com>
2023-11-16 00:47:47 -0500

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KnexModule } from './global/modules/knex.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './userRelated/user/user.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { DishModule } from './dish/dish.module';
 import { RestaurantDishModule } from './restaurantDish/restaurantDish.module';
