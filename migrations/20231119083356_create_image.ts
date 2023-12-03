f3f2803b0ded5e4b37b434b0189925143ea1d6ec
ttiimmothy <dinosauli2006@gmail.com>
2023-12-03 17:20:59 -0500

    feat: coverage and test migrations update

--- migrations/20231119083356_create_image.ts
+++ migrations/20231119083356_create_image.ts
@@ -1,28 +1,29 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  if (!(await knex.schema.hasTable('menu_photo'))) {
-    await knex.schema.createTable('menu_photo', (table) => {
+  if (!(await knex.schema.hasTable('image'))) {
+    await knex.schema.createTable('image', (table) => {
       table
-        .uuid('menu_photo_id')
+        .uuid('image_id')
         .primary()
         .defaultTo(knex.raw('gen_random_uuid()'))
         .notNullable();
-      table.uuid('restaurant_id').notNullable();
-      table.foreign('restaurant_id').references('restaurant.restaurant_id');
+      table.text('image_url').notNullable();
+      table.uuid('image_category_id').notNullable();
+      table
+        .foreign('image_category_id')
+        .references('image_category.image_category_id');
       table.uuid('photo_category_id').notNullable();
       table
         .foreign('photo_category_id')
         .references('photo_category.photo_category_id');
-      table.text('photo_url').notNullable();
-      table.uuid('image_id');
-      table.foreign('image_id').references('image.image_id');
       table.boolean('active').notNullable().defaultTo(true);
       table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+      table.timestamp('modified_at').notNullable().defaultTo(knex.fn.now());
     });
   }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('menu_photo');
+  await knex.schema.dropTableIfExists('image');
 }
