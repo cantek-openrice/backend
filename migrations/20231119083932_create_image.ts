200569e64261db40cfcc75f7a48a8722ae1fe163
ttiimmothy <dinosauli2006@gmail.com>
2023-12-04 12:08:44 -0500

    feat: coverage

--- migrations/20231119083932_create_image.ts
+++ migrations/20231119083932_create_image.ts
@@ -30,5 +30,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('user');
+  await knex.schema.dropTableIfExists('image');
 }
